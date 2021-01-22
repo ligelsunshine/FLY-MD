2eee3de742652c11fd10ee9adb201188fc01e60a
ligel <ligelyqz@163.com>
2021-01-22 11:31:55 +0800

    完善配置扫描路径，这里也是卡我半天

--- server/src/main/java/com/fly/server/Application.java
+++ server/src/main/java/com/fly/server/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value ={"com.fly.common.**","com.fly.server.**"})
+@ComponentScan(value ={"com.fly.common.**","com.fly.server.**.**"})
 @MapperScan({"com.fly.server.**.mapper"})
 public class Application {
     public static void main(String[] args) {
