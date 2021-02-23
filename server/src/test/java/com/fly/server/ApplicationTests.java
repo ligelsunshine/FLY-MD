3a8725c1c885f22fd63393187474d9760d9befba
ligel <ligelyqz@163.com>
2021-02-23 14:48:40 +0800

    增加自动生成id算法测试

--- server/src/test/java/com/fly/server/ApplicationTests.java
+++ server/src/test/java/com/fly/server/ApplicationTests.java
@@ -1,8 +1,15 @@
 package com.fly.server;
 
-import com.fly.server.learndemo.BeanDemo;
+import com.fly.common.utils.IdGen;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -14,9 +21,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class ApplicationTests {
     @Test
-    void contextLoads() {
-        BeanDemo beanDemo= new BeanDemo();
-        beanDemo.helloBean("ligel");
+    void IdGeneratorTest() {
+        long avg = 0;
+        for (int k = 0; k < 10; k++) {
+            List<Callable<Long>> partitions = new ArrayList<Callable<Long>>();
+            final IdGen idGen = IdGen.get();
+            for (int i = 0; i < 1400000; i++) {
+                partitions.add(new Callable<Long>() {
+                    @Override
+                    public Long call() throws Exception {
+                        return idGen.nextId();
+                    }
+                });
+            }
+            System.out.println("id数量："+partitions.size());
+            ExecutorService executorPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+            try {
+                long s = System.currentTimeMillis();
+                executorPool.invokeAll(partitions, 10000, TimeUnit.SECONDS);
+                long s_avg = System.currentTimeMillis() - s;
+                avg += s_avg;
+                System.out.println("完成时间需要: " + s_avg / 1.0e3 + "秒");
+                executorPool.shutdown();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("平均完成时间需要: " + avg / 10 / 1.0e3 + "秒");
 
     }
 
